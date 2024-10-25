85a122fc2c3fe17ac53d5d96323c840f342b3ce2
fridaymaguilar <fridaymaguilar@gmail.com>
2024-10-24 22:03:22 -0700

    : adding

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -16,8 +16,8 @@ age = 2015 - year;
 //output 
 document.write("Make:"+ make + "<br>" );
 document.write("Model:"+ model + "<br>");
-document.write("Color"+ color + "<br>");
-document.write("Year"+ year + "<br>");
-document.write("Age"+ age + "years<br>");
+document.write("Color:"+ color + "<br>");
+document.write("Year:"+ year + "<br>");
+document.write("Age:"+ age + "years<br>");
 
 console.log("Here is a message"); 
