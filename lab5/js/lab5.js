6c8ac870ff8f17469a2c21b3ff9b02f28a16b999
fridaymaguilar <fridaymaguilar@gmail.com>
2024-10-24 20:55:51 -0700

    js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,35 @@
+// index.js - purpose and description here
+// Author: Frida Aguilar 
+// Date: October 24 2024 
+// lisence: Public Domain 
+//
+
+// 
+make = "Ford"; 
+model = "explorer";
+color = "Rusty";
+year = 1995; 
+
+// calculate 
+age = 2015 - year;
+
+//output 
+document.writeIn("Make:"+ make + "<br>" )
+document.writeIn("Model:"+ model + "<br>")
+document.writeIn("Model:"+ model + "<br>")
+document.writeIn("Color"+ color + "<br>")
+document.writeIn("Age"+ age + "years<br>")
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
