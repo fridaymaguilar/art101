40cc7343ca4ea591b0bfa572bc79bc8debfc9c94
fridaymaguilar <fridaymaguilar@gmail.com>
2024-10-24 22:09:25 -0700

    edits

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -6,7 +6,7 @@
 
 // Define Variables 
 make = "Ford"; 
-model = "explorer";
+model = "Explorer";
 color = "Rusty";
 year = 1995; 
 
@@ -14,10 +14,10 @@ year = 1995;
 age = 2015 - year;
 
 //output 
-document.write("Make:"+ make + "<br>" );
-document.write("Model:"+ model + "<br>");
-document.write("Color:"+ color + "<br>");
-document.write("Year:"+ year + "<br>");
-document.write("Age:"+ age + "years<br>");
+document.write("Make: "+ make + "<br>" );
+document.write("Model: "+ model + "<br>");
+document.write("Color: "+ color + "<br>");
+document.write("Year: "+ year + "<br>");
+document.write("Age: "+ age + " years<br>");
 
 console.log("Here is a message"); 
