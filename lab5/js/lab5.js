4d5d2998f0095e3dc6b64efc96fe38b8d8dcd3e3
fridaymaguilar <fridaymaguilar@gmail.com>
2024-10-24 21:10:57 -0700

    fix? please please please

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -4,7 +4,7 @@
 // lisence: Public Domain 
 //
 
-// 
+// Define Variables 
 make = "Ford"; 
 model = "explorer";
 color = "Rusty";
@@ -20,16 +20,3 @@ document.writeIn("Model:"+ model + "<br>")
 document.writeIn("Color"+ color + "<br>")
 document.writeIn("Age"+ age + "years<br>")
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
