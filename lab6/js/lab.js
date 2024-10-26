4ef78deb465e416288180829a8a690952374f13b
fridaymaguilar <fridaymaguilar@gmail.com>
2024-10-25 20:09:34 -0700

    fix

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -5,19 +5,21 @@
 //
 
 // Define Variables 
-make = "Ford"; 
-model = "Explorer";
-color = "Red";
-year = 1995; 
+MyTransprot = ["Ford Explorer","Bike", "Bus", "Walking"];
 
-// calculate 
-age = 2015 - year;
+//create an object for main ride 
+MyMainRide = {
+  make = "Ford",
+  model = "Explorer",
+  color = "Red",
+  year = 1995,
+
+age : fuction() {
+    return 2022 - this.year;
+  }
+}
 
 //output 
-document.write("Make: "+ make + "<br>" );
-document.write("Model: "+ model + "<br>");
-document.write("Color: "+ color + "<br>");
-document.write("Year: "+ year + "<br>");
-document.write("Age: "+ age + " years<br>");
-
-console.log("Here is a message"); 
+document.write("Kind of transportation I use:", myTransport, "</br>");
+document.write(My Main Ride: <pre>",
+    JSON.stringify(MyMainRide, null,'/t'), "</pre>");
