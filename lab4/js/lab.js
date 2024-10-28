28a41ecc9b4845b1828e02d603f2364c016366ac
fridaymaguilar <fridaymaguilar@gmail.com>
2024-10-27 23:41:01 -0700

    html

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// How_to_sew_ 
+??
+// Author: Frida Aguilar 
+// Date: October 27 2024 
 
 // Constants
 
 // Functions
+
+//Select Thread and Needle
+//Cut the Thread
+//Thread the Needle
+//Knot the thread
+//Prepare Your Fabric
+//Start Sewing 
+//Make Your Stitches
+//Finish Your Stitching
+// Cut the Thread
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
