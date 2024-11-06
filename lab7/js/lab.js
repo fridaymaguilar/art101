1bb7535a208e435740f0d2cd53eafd2f2ae8cb4b
fridaymaguilar <fridaymaguilar@gmail.com>
2024-11-05 20:56:36 -0800

    fix

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -31,14 +31,16 @@ function sortUserName() {
     return 
     nameSorted; 
     
-
+}
  
   // output
-  7
   document.writeln("Oh hey, I've fixed your name: ",
-  8
-  sortUserName (), "</br>") 
+        sortUserName (), "</br>") 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
+
+
+
+
 function myFunction(param1, param2) {
     // some code here
     // return results;
