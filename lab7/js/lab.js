6df4a129222892aa5a1059d384c22451729115a9
fridaymaguilar <fridaymaguilar@gmail.com>
2024-11-05 23:15:15 -0800

    fix

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,6 +2,62 @@
 // Author: Frida Aguilar 
 // Date: November 5 2024 
 
+
+// shuffleArray - take an array and shuffle the contents
+// Thanks to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+// given a string, return string in Title Case
+// thanks to https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function sortUserName(userName) {
+
+    return userName.toLowerCase().split("").sort().join("");
+}
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function randomizeName(userName) {
+    // convert userName string to an array
+    var nameArray = userName.toLowerCase().split("");
+    console.log("nameArray =", nameArray);
+    // shuffle array with our shuffle function
+    var shuffledArray = shuffleArray(nameArray);
+    console.log("shuffledArray =", shuffledArray);
+    var shuffledString = shuffledArray.join("");
+    // shift to Title Case (like a name)
+    var newName = toTitleCase(shuffledString);
+    // return array to a string
+    return newName;
+}
+
+
+
 function sortUserName() {
   var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
   console.log("userName =", userName);
