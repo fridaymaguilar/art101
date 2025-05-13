756b3f0ba989707b7193e8adb62eb2dd015299b0
fridaymaguilar <fridaymaguilar@gmail.com>
2025-05-12 21:28:49 -0700

    fix

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,27 +4,27 @@
   Author: Frida Aguilar 
   Date: May 12 2025 
 */
-// Add button to the challenge section
+
+// Add a button to the challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
-// Add button to the problems section
+// Add a button to the problems section
 $("#problems").append("<button id='button-problems'>Make Special</button>");
 
-// Add button to the results section
+// Add a button to the results section
 $("#results").append("<button id='button-results'>Make Special</button>");
 
-// Add click listener for challenge button
-$("#button-challenge").click(function(){
+// Add click listener for the challenge button
+$("#button-challenge").click(function() {
   $("#challenge").toggleClass("special");
 });
 
-// Add click listener for problems button
-$("#button-problems").click(function(){
+// Add click listener for the problems button
+$("#button-problems").click(function() {
   $("#problems").toggleClass("special");
 });
 
-// Add click listener for results button
-$("#button-results").click(function(){
+// Add click listener for the results button
+$("#button-results").click(function() {
   $("#results").toggleClass("special");
-}); 
-
+});
