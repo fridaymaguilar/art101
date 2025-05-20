b0bf739d8ec5c96f7a3c42e20dec91337bb1eac6
fridaymaguilar <fridaymaguilar@gmail.com>
2025-05-19 18:32:19 -0700

    fix

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -6,21 +6,18 @@
 */
 
 
-// craete a web chat 
+// Function that returns a random slice of lorem ipsum text
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  return text.slice(randStart, randStart + randLen);
+}
 
-//make a box for the convo 
-
-// add the putton 
-
-//user types something in the text box
-
-// on click even listener 
-
-//make the button copy the text 
-    //find the box 
-
-    // and its contects
-
-    // copy the contects 
-    
-    // copy where 
+// Add click event to the button
+$("#make-convo").click(function() {
+  const newText = generateRandomText();
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
