1cfffe3f66a902c26ce79004cee874c86071ecc4
fridaymaguilar <fridaymaguilar@gmail.com>
2025-05-19 18:45:21 -0700

    fix

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -6,10 +6,14 @@
 */
 
 function generateRandomText() {
-  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...";
-  const min = 3;
-  const max = 100;
+  const text = "Lorem ipsum dolor sit amet..."; // long text
+  const min = 3, max = 100;
   const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
   const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
   return text.slice(randStart, randStart + randLen);
 }
+
+$("#make-convo").click(function() {
+  const newText = generateRandomText();
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
