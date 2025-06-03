82fff10beb9f43974a6dc5cc5237397e65b22a8a
fridaymaguilar <fridaymaguilar@gmail.com>
2025-06-02 17:53:19 -0700

    fix

--- lab15 copy/js/lab.js
+++ lab15 copy/js/lab.js
@@ -1,12 +1,11 @@
 // Author: Frida Aguilar
 // Date: June 2, 2025
 // Purpose: Fetch data using AJAX from the yesno.wtf API and display it
-
 const playlistId = '0syfCV3pBzv4MCLAa0dMls';
 
 <iframe
-  title="Spotify Embed: Recommendation Playlist "
-  src={`https://open.spotify.com/embed/playlist/0syfCV3pBzv4MCLAa0dMls?utm_source=generator&theme=0`}
+  title="Spotify Embed: Recommendation Playlist"
+  src={`https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=0`}
   width="100%"
   height="100%"
   style={{ minHeight: '360px' }}
--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -22,3 +22,16 @@ $("#activate").click(function() {
     }
   });
 });
+
+const playlistId = '0syfCV3pBzv4MCLAa0dMls';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist"
+  src={`https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
