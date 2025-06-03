06aae3ddb98f3298e0530d642f6e3b3357ffc409
fridaymaguilar <fridaymaguilar@gmail.com>
2025-06-02 17:38:17 -0700

    fix

---
+++ Test/js/lab.js
@@ -0,0 +1,15 @@
+// Author: Frida Aguilar
+// Date: June 2, 2025
+// Purpose: Fetch data using AJAX from the yesno.wtf API and display it
+const playlistId = '0syfCV3pBzv4MCLAa0dMls';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/0syfCV3pBzv4MCLAa0dMls?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
